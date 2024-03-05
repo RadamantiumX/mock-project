778b00f7ed866b141e6287cf5e29fb502d431b62
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-03-05 19:28:14 -0300

    adding changes

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom"
 export default function NotFound() {
     return (
         <>
          <h1>404 - NOT FOUND</h1>
+         <Link to="/">To main page</Link>
         </>
     )
 }
--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
+import Element from "./pages/Element";
 
 const router = createBrowserRouter([
      {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
      {
         path: '/profile',
         element: <Profile/>
+     },
+     {
+        path: '/element',
+        element: <Element/>
      },
      {
         path: '*',
