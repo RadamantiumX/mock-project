45361a7d75cfd23453692a76341d4bc047755306
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-03-08 00:03:47 -0300

    new content

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,13 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+      
+  ],
 }
 
