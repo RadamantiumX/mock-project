39dda4bd82e0deede680d481846f6edc92b74967
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-03-12 00:03:44 -0300

    Handling empty responses to API

--- src/components/videoComponents/SideBarVideos.tsx
+++ src/components/videoComponents/SideBarVideos.tsx
@@ -2,6 +2,7 @@ import { getRelatedVideos } from "../../services/resources"
 import { useEffect, useState } from "react"
 import { type Video } from "../../types/eporner"
 import { Link } from "react-router-dom"
+import { Ghost } from "../icons/Ghost"
 
 interface Props {
     keywords: string
@@ -10,19 +11,32 @@ interface Props {
 // Related Videos displayed
 export const SideBarVideos:React.FC<Props> = ({ keywords }) => {
   const [videos, setVideos] = useState<Video[]>([])  
+ const [counter, setCounter] = useState<number>(0)
+ const [loading, setLoading] = useState(true)
   const lowerCase = keywords.toLowerCase()
 
   const stringSplit = lowerCase.split(" ")
-    
+  const handleResults = () => {
+    setCounter(counter + 1)
+    getRelatedVideos(stringSplit[counter])
+               .then((data) => {
+               setVideos(data)
+            }             
+    )
+  } 
   useEffect(() => {
-  getRelatedVideos(stringSplit[2]).then((data) => setVideos(data))
-
-  },[])
+  getRelatedVideos(stringSplit[counter])
+               .then((data) => {
+               setVideos(data)
+               setLoading(false)
+            }             
+    ) 
+},[])
 
   return (
     <aside>
         <h3>Related Videos</h3>
-        <div>
+        {loading ? <div>Cargando</div> : <div> {videos.length !== 0 ?<div>
             {videos.map((video,index) => (
                 <article key={index}>
                     <Link className="gap-2" to={`/video/${video.id}/${video.keywords}`} target="_blank">
@@ -31,10 +45,12 @@ export const SideBarVideos:React.FC<Props> = ({ keywords }) => {
                     </Link>
                 </article>
             ))}
-        </div>
+        </div> : <div>
+               <h3>We can't found related videos <Ghost/></h3>
+               <button onClick={handleResults}>Cargar más resultados</button>
+            </div>}</div>}
     </aside>
   )
 }
 
 // Some example response: ', creampie, japanese, old man, blowjob, big tits, milf, Forbidden Care Suikawa Yuri 476'
-
