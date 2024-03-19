0fcc9f08ec2e689d8fbfcd29904f79aba987ad22
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-03-18 23:05:37 -0300

    adding new input component seach to navbar

--- src/components/layoutComponents/NavBar.tsx
+++ src/components/layoutComponents/NavBar.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Logo from "../../assets/project/dh-logo-preview.png";
 import { useState } from "react";
+import { QueryForm } from "./QueryForm";
 import "./navbar.scss";
 
 export default function NavBar() {
@@ -28,18 +29,7 @@ export default function NavBar() {
             </div>
           </div>
           <div className='mb-4 w-full md:mb-0 md:w-1/4'>
-            <div className="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
-              <div className="grid place-items-center h-full w-12 text-gray-300">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                </svg>
-              </div>
-              <input
-                className="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
-                type="text"
-                id="search"
-                placeholder="Search something.." />
-            </div>
+            <QueryForm/>
           </div>
           {isMenuOpen && (
             <div className="absolute top-full left-0 w-full bg-gray-900 z-50">
