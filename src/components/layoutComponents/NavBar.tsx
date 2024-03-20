a59fd7bd4a96a79651fcaa57f35073226579ea15
Juan Arede <85207414+juanarede@users.noreply.github.com>
2024-03-20 14:04:50 -0300

    Dropdown-Navbar  Models

--- src/components/layoutComponents/NavBar.tsx
+++ src/components/layoutComponents/NavBar.tsx
@@ -11,6 +11,17 @@ export default function NavBar() {
     setIsMenuOpen(!isMenuOpen);
   };
 
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsOpen(false);
+  };
+
   return (
     <>
       <nav className="relative ">
@@ -29,7 +40,7 @@ export default function NavBar() {
             </div>
           </div>
           <div className='mb-4 w-full md:mb-0 md:w-1/4'>
-            <QueryForm/>
+            <QueryForm />
           </div>
           {isMenuOpen && (
             <div className="absolute top-full left-0 w-full bg-gray-900 z-50">
@@ -51,7 +62,41 @@ export default function NavBar() {
             <ul className="flex flex-row gap-4 mobile-menu">
               <li><Link className="subnav" to="/">Home</Link></li>
               <li><Link className="subnav" to="/categories">Categories</Link></li>
-              <li><Link className="subnav" to="/models">Models</Link></li>
+
+           {/* /////////   DropDown Models ///////// */}
+          {/* ////Las imagens se van a mostrar en filas de 4 (4 arriba y 4 abajo)//// */}
+              <div className="dropdown inline-block relative" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                <Link to="" className="subnav rounded inline-flex items-center">
+                  Modelos
+                  <svg className="fill-current h-4 w-4 mt-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                  </svg>
+                </Link>
+                <div className={`dropdown-menu absolute ${isOpen ? '' : 'hidden'} text-gray-700 pt-1`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={{ zIndex: 100, width: '600px', maxHeight: '600px', overflowY: 'auto' }}>
+                  <div className="bg-gray-900 p-4">
+                    <div className="grid grid-cols-4 gap-4">
+                      {/* fila de imágenes */}
+                      <div className="rounded-lg overflow-hidden">
+                        <div className="wrapper antialiased text-gray-900">
+                          <div style={{ width: '150%', height: '100%' }}>
+                            <img src="https://source.unsplash.com/random/400x400" alt="imagen aleatoria" className="w-full h-full object-cover object-center rounded-lg shadow-md" />
+                          </div>
+                        </div>
+                      </div>
+
+                      {/*fin fila imagenes */}
+                    </div>
+                    {/* Botón de "Ver más modelos" */}
+                    <div className="text-center mt-4 mb-2">
+                      <Link to="/mas-modelos" className="bg-pink-600 hover:bg-pink-500 text-white font-bold py-2 px-4 rounded transition duration-300">
+                        More Models
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+               {/* fin dropdownModels /////////////////////////////// */}
+
               <li><Link className="subnav" to="/photos">Photos</Link></li>
             </ul>
           </nav>
