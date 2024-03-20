05f00444c0615ec731341715e1e65494ba60c8db
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-03-19 22:12:25 -0300

    Creating new pages files

---
+++ src/pages/Categories.tsx
@@ -0,0 +1,73 @@
+
+
+export default function Categories() {
+  return (
+    <div>Categories</div>
+  )
+}
+/**
+ * ❗ Estas son todas las categorias
+ * 
+ * Anal
+ * Japanese
+ * HD Porn 1080p
+ * Teen
+ * Asian
+ * Milf
+ * Big tits
+ * VR Porn
+ * Ebony
+ * Gay
+ * Amateur
+ * Lesbian
+ * Shemale
+ * Mature
+ * BBW
+ * POV
+ * Creampie
+ * Interracial
+ * Hentai
+ * Big Ass
+ * Threesome
+ * Hardcore
+ * Homemade
+ * Latina
+ * Blowjob
+ * Indian
+ * BDSM
+ * Orgy
+ * Vintage
+ * Big Dick
+ * Blonde
+ * Masturbation
+ * Group Sex
+ * Cumshot
+ * Petite
+ * Toys
+ * Public
+ * Fat
+ * Massage
+ * Webcam
+ * Double Penetration
+ * Lingerie
+ * Squirt
+ * Brunnete
+ * Older man
+ * Redhead
+ * Office
+ * Outdoor
+ * Handjob
+ * Bukkake
+ * Nurses
+ * Bondage
+ * Footjob
+ * Fisting
+ * Small Tits
+ * Uniform
+ * Striptease
+ * Bisexual
+ * Housewife
+ * Doctor
+ * ASMR
+ * 
+ */
---
+++ src/pages/Video4k.tsx
@@ -0,0 +1,7 @@
+
+export default function Video4k() {
+  return (
+    <div>Video4k</div>
+  )
+}
+
