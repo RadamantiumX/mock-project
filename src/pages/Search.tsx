252a303694b77afcf67b06fb45977cc290c7607e
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-03-20 23:31:19 -0300

    adding content to argument

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,31 +1,41 @@
 import { VideosResults } from "../components/searchComponents/VideosResults"
 import { useParams } from "react-router-dom"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { NotResults } from "../components/commonComponents/NotResults"
 import { useAppDispatch, useAppSelector } from "../redux/hooks"
 import { getSource } from "../redux/sourceSlice"
+import { LoadButton } from "../components/commonComponents/LoadButton"
 
 type Params = {
   query: string
 }
 export default function Search() {
+  const [counter, setCounter] = useState<number>(7)
   const { query } = useParams<Params>()
-
+  
   const dispatch = useAppDispatch()
   const source = useAppSelector(state => state.source.data)
 
-  useEffect(()=>{
-    // Prevent EXTRA typing ♻
-    if (query !== undefined) {
-    dispatch(getSource(query))
+  const handleResults = () =>{
+    setCounter(counter + 7)
   }
-  },[])
+  
+  useEffect(()=>{ 
+    const payload = query?.concat(" ", counter.toString())
+    // Prevent EXTRA typing ♻
+    if (query !== undefined ) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        dispatch(getSource(payload as any))
+    
+  }
+
+  },[query, counter]) // ✅Component data is refreshed when "query" is updated
 
   return (
     <main>
       <h1 className="text-2xl">Results of search for: <span className="font-bold">{query}</span></h1>
       {
-      source?.length !== 0 ? <VideosResults source={source}/>
+      source?.length !== 0 ? <div><VideosResults source={source}/><LoadButton onClick={handleResults}/></div>
       : <NotResults/>
       }
     </main>
