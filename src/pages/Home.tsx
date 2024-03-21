59c211aa40236366efd1bd165875326786d3e12f
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-03-20 23:31:47 -0300

    adding new button component to load more videos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,29 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import {Header} from "../components/homeComponents/Header"
 import { Videos } from "../components/homeComponents/Videos"
 import { useAppDispatch, useAppSelector } from "../redux/hooks"
 import { getSource } from "../redux/sourceSlice"
-
+import { LoadButton } from "../components/commonComponents/LoadButton"
 
 export default function Home () {
-
+  const [counter, setCounter] = useState<number>(12)
   const dispatch = useAppDispatch()
   const source = useAppSelector(state => state.source.data)
   
+  const handleResults = () => {
+    setCounter(counter + 10)
+  }
+
   useEffect(() => {
-    dispatch(getSource(null))
+    
+    dispatch(getSource(counter))
    
-  },[])
+  },[counter])
     return(
         <main>
           <Header/>
           <Videos source={source}/>
+         <LoadButton onClick={handleResults}/>
         </main>
     )
 }
