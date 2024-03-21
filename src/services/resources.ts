d033a24669c799ccbfbf5db20ce652256dd6d78e
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-03-20 23:28:43 -0300

    fetch modify to set more videos in the query

--- src/services/resources.ts
+++ src/services/resources.ts
@@ -2,21 +2,25 @@ import {  type APIEpornerResponse } from "../types/eporner"
 
 // const configValue : string = (process.env.EPORNER_API_URL as string)
 
-export const getLatestContent = async () => {
-   
-    const res = await fetch(`${import.meta.env.VITE_EPORNER_API_URL}api/v2/video/search/?query=popular&per_page=12&page=2&thumbsize=big&order=latest&gay=1&lq=1&format=json`)
+export const getLatestContent = async ( payload: number = 12 ) => {
+    const qty = payload.toString();
+    const res = await fetch(`${import.meta.env.VITE_EPORNER_API_URL}api/v2/video/search/?query=popular&per_page=${qty}&page=1&thumbsize=big&order=latest&gay=1&lq=1&format=json`)
     const {videos: data} = await res.json() as APIEpornerResponse
    
     return data
 
 }
 
-export const getRelatedVideos = async ( params: string ) => {
-   
-    const res = await fetch(`${import.meta.env.VITE_EPORNER_API_URL}api/v2/video/search/?query=${params}&per_page=7&page=2&thumbsize=big&order=latest&gay=1&lq=1&format=json`)
+// This function needs separate two values to modify the API response
+export const getRelatedVideos = async ( payload:string) => {
+    let qty = 7 // Default value to set quantity of results by request
+    const paramsArray = payload.split(' ') // Splits params in 2 elements
+    qty = qty + parseInt(paramsArray[1]) // Seting new value
+    const res = await fetch(`${import.meta.env.VITE_EPORNER_API_URL}api/v2/video/search/?query=${paramsArray[0]}&per_page=${qty.toString()}&page=2&thumbsize=big&order=latest&gay=1&lq=1&format=json`)
     const {videos: data} = await res.json() as APIEpornerResponse
     return data
   
 }
+
 
 // https://www.eporner.com/
