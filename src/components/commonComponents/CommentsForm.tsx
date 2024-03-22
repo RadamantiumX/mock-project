b1925bcf873d6a06ecb44673a403d849a27c324c
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-03-22 00:03:52 -0300

    change file name - before: CommentFomr - After: PostCommentForm

--- src/components/commonComponents/CommentsForm.tsx
+++
@@ -1,9 +0,0 @@
-export const CommentsForm = () => {
-  return (
-    <form action="">
-        <textarea name="" id="" cols="30" rows="10">
-        </textarea>
-        <button>Send</button>
-    </form>
-  )
-}
