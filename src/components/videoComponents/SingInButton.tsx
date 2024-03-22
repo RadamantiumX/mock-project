4ce43288a8c31b63ea3911d5256c838479c5a487
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-03-21 23:58:58 -0300

    create signin button

---
+++ src/components/videoComponents/SingInButton.tsx
@@ -0,0 +1,9 @@
+
+export const SingInButton = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <button className="border rounded-md bg-red-700 p-2 hover:bg-red-600">Sing In to comment</button>
+    </div>
+  )
+}
+
