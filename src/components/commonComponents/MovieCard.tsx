94f4d49d2be1104be0c531d6e740d07ec87a8c9a
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-03-22 00:02:29 -0300

    import eye icon

--- src/components/commonComponents/MovieCard.tsx
+++ src/components/commonComponents/MovieCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Eye } from '../icons/Eye'
 import './movieCard.scss'
 
 /**
@@ -23,8 +24,7 @@ interface Props {
 
 export const MovieCard:React.FC<Props> = ({ title, image, id, lengthMin, views, keywords }) => {
   return (
-    <>
-   
+    <> 
           <div className="block relative group transition max-w-sm mx-auto mt-3 mb-4">
               <Link to={`/video/${id}/${keywords}/${title}/${views}`} className="block">
                   <div className="aspect-w-2 aspect-h-1 rounded-2xl shadow overflow-hidden bg-gray-100 relative">
@@ -43,13 +43,7 @@ export const MovieCard:React.FC<Props> = ({ title, image, id, lengthMin, views, 
                           </h3>
                           <span className="mt-1 shrink-0 text-xs inline-flex items-center gap-1">
                               {views}
-                              <svg className="w-3 h-3 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                                  fill="currentColor" aria-hidden="true">
-                                  <path d="M10 12a2 2 0 100-4 2 2 0 000 4z"></path>
-                                  <path fillRule="evenodd"
-                                      d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                                      clipRule="evenodd"></path>
-                              </svg>
+                              <Eye size={'w-3 h-3 '}/>
                           </span>
                       </div>
                   </div>
