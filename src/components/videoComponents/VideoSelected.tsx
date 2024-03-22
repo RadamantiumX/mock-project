34b41844d5ede2883173dda6721805f82cc92c94
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-03-21 23:58:39 -0300

    import icon comp and seting size prop

--- src/components/videoComponents/VideoSelected.tsx
+++ src/components/videoComponents/VideoSelected.tsx
@@ -1,4 +1,5 @@
 import './videoSelected.scss'
+import { Eye } from '../icons/Eye';
 
 interface Props {
     id?: string;
@@ -12,23 +13,22 @@ export const VideoSelected:React.FC<Props> = ({ id, title, views }) => {
 
   const truncateTitle = () => {
     const words = title?.split(' ');
+    if (words !== undefined) {
     if (words?.length > MAX_TITLE_WORDS || words !== undefined) {
       return words?.slice(0, MAX_TITLE_WORDS).join(' ') + '...';
     }
+  }
     return title;
   };
-
+// w-6 h-6 
   return (
     <>
-<div style={{marginTop:"4rem"}} className="mr-10">
-  <section className="mt-7 mb-7 ml-10 h-full relative">
+<section style={{marginTop:"4rem"}} className="mr-10">
+  <div className="mt-7 mb-7 ml-10 h-full relative">
   <h2 style={{fontSize:"1.7rem"}} className="flex-1 text-xl md:text-2xl font-bold text-gray-100 mb-5 truncate-multiline font-title">
   {truncateTitle()}
   <span style={{verticalAlign: "middle"}} className="mt-1 shrink-0 ml-3 text-sm sm:text-lg inline-flex items-center gap-2">
-    <svg className="w-6 h-6 sm:w-5 sm:h-5 text-gray-500 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" style={{verticalAlign: "middle"}}>
-      <path d="M10 12a2 2 0 100-4 2 2 0 000 4z"></path>
-      <path fillRule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clipRule="evenodd"></path>
-    </svg>
+   <Eye size={'w-6 h-6 '}/>
     <span style={{verticalAlign: "middle"}}>{views}</span>
   </span>
 </h2>
@@ -43,8 +43,8 @@ export const VideoSelected:React.FC<Props> = ({ id, title, views }) => {
               ></iframe>
             </div>
           </div>
-  </section>
-</div>
+  </div>
+</section>
 
 </>
   )
