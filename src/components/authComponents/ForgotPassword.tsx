e34cbfeddf6fe9ab62efe52218fd6bdc7cfc0494
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-03-30 18:19:34 -0300

    fixing and text adding

--- src/components/authComponents/ForgotPassword.tsx
+++ src/components/authComponents/ForgotPassword.tsx
@@ -4,6 +4,7 @@ export const ForgotPassword = () => {
   return (
     <form className="flex flex-col gap-2 w-1/2" action=""> 
        <h1 className="text-3xl">Forgot Password</h1>
+       <p>We'll send you a link with the email address registered.</p>
       <input type="email" placeholder="email"/> 
       <button className="border rounded-md">Send Email</button>
       <Link to="/auth/portal/signup">¿Not registered? create an account</Link>
--- src/components/modelsComponents/SelectModels.tsx
+++ src/components/modelsComponents/SelectModels.tsx
@@ -19,7 +19,7 @@ export default function SelectModels() {
           {/* ////Las imagens se van a mostrar en filas de 4 (4 arriba y 4 abajo)//// */}
           <div className="dropdown inline-block relative" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
           <Link to="/models" className="subnav rounded inline-flex items-center">
-            Modelos
+            Models
             <svg className="fill-current h-4 w-4 mt-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
               <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
             </svg>
