f1eef2d85d5035633dc74d5cc97b4ad085a2a56b
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-03-29 23:42:25 -0300

    fixing the graphql client

--- src/gqlClient/client.ts
+++ src/gqlClient/client.ts
@@ -1,17 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client"
 
-const getAuth = () => {
-    const token:any= localStorage.getItem('vanilla-leaks-token')
+const getAuth = ():any => {
+    const token = localStorage.getItem('vanilla-leaks-token')
     return token ? `bearer ${token}` : null
 }
 
 export const client = new ApolloClient({
+    
     connectToDevTools: true,
     cache: new InMemoryCache(),
     link: new HttpLink({
         headers: {
-            authorization: getAuth()
+           authorization: getAuth()
         },
-        uri: 'http://localhost:4000'
+        uri: 'http://localhost:4000/graphql'
     })
 })
