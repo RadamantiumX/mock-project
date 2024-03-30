de495c8b1c1e18a9478261ecee9a9ce853c4b07d
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-03-29 23:43:20 -0300

    fix the plural word

--- src/components/homeComponents/Header.tsx
+++ src/components/homeComponents/Header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
     
     <button type="button" className="ml-4 text-white bg-[#dd2471] hover:bg-[#dd2471]/80 focus:ring-4 focus:ring-[#FF9119]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#dd2471]/80 dark:focus:ring-[#dd2471]/40 mr-2 mb-2">
       <i className="fa-solid fa-play mr-2"></i>
-    4K VIDEO
+    4K VIDEOS
     </button>
 
   )
