6063750994cf464833c483a4a366c28bb22ea948
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-03-31 00:15:27 -0300

    delete graphql client provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,16 @@ import { RouterProvider } from 'react-router-dom'
 import { ContextProvider } from './contexts/ContextProvider.tsx'
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
-import { client } from './gqlClient/client.ts'
-import { ApolloProvider } from '@apollo/client'
+// import { client } from './gqlClient/client.ts'
+// import { ApolloProvider } from '@apollo/client'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <ApolloProvider client={client}>
+    
     <Provider store={store}>
     <ContextProvider>
       <RouterProvider router={router}/>
     </ContextProvider>
     </Provider>
-    </ApolloProvider>
+    
   ,
 )
