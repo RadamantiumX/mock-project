34ce56fdb8dbe287e31d38e4a57db4e5e531d140
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-04-03 23:56:39 -0300

    set token on the layout

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -1,6 +1,16 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Navigate } from "react-router-dom"
+import { useStateContext } from "../contexts/ContextProvider"
 
 export default function AuthLayout() {
+ const { token } = useStateContext()
+
+
+  
+  if(token){
+    return <Navigate to="/"/>
+  }
+
+
   return (
     <>
     <Outlet/>
