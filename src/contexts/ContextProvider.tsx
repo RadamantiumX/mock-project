b74413fe3722c3e7046866d96aa51e4450b3e09b
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-04-03 23:57:00 -0300

    set new state on the context

--- src/contexts/ContextProvider.tsx
+++ src/contexts/ContextProvider.tsx
@@ -1,19 +1,36 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { createContext, useContext, useState, PropsWithChildren, SetStateAction, Dispatch } from "react";
 
 type ContextType = {
-    tags: string[],
-    setTags: Dispatch<SetStateAction<string[]>>
+    token: string | null,
+    setToken: Dispatch<SetStateAction<string>>,
+    id: number | null,
+    setId: Dispatch<SetStateAction<number>>
 }
 
 const StateContext = createContext<ContextType>({
-    tags: [],
-    setTags: () => {}
+    token: null,
+    id: null,
+
+    setToken: () => {},
+    setId: () => {}
 })
 
 export const ContextProvider = ({ children }:PropsWithChildren) => {
-    const [tags, setTags] = useState<string[]>([]) 
+    const [token, _setToken] = useState(localStorage.getItem('auth-token')) 
+    const [id, setId] = useState<any>(null)
+
+    const setToken = (token:any) => {
+        _setToken(token)
+        if(token){
+            localStorage.setItem('auth-token', token)
+        }else{
+            localStorage.removeItem('auth-token')
+        }
+    }
+
     return (
-        <StateContext.Provider value={{ tags, setTags }}>
+        <StateContext.Provider value={{ token, setToken, id, setId }}>
             {children}
         </StateContext.Provider>
     )
