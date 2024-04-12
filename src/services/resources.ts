b4fe7787126bab0476f34fd4af2e055da55c3926
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-04-12 20:02:37 -0300

    testing ortintation for user

--- src/services/resources.ts
+++ src/services/resources.ts
@@ -2,9 +2,12 @@ import {  type APIEpornerResponse } from "../types/eporner"
 
 // const configValue : string = (process.env.EPORNER_API_URL as string)
 
+
 export const getLatestContent = async ( payload: number = 12 ) => {
+    // Traer un numero de los parametros, si es menor o igual a 1, aumentamos "binary", sino, aumentamos "qty"
+    let binary = 0
     const qty = payload.toString();
-    const res = await fetch(`${import.meta.env.VITE_EPORNER_API_URL}api/v2/video/search/?query=popular&per_page=${qty}&page=1&thumbsize=big&order=latest&gay=1&lq=1&format=json`)
+    const res = await fetch(`${import.meta.env.VITE_EPORNER_API_URL}api/v2/video/search/?query=popular&per_page=${qty}&page=1&thumbsize=big&order=latest&gay=${binary.toString()}&lq=1&format=json`)
     const {videos: data} = await res.json() as APIEpornerResponse
    
     return data
