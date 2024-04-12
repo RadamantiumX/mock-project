c48cc40830eacb4966d1810609dcfabd030abc12
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-04-12 20:04:58 -0300

    testing

---
+++ src/components/commonComponents/UserButton.tsx
@@ -0,0 +1,10 @@
+export const UserButton = (props: {onClick:()=>void, nickname: string}) => {
+  return (
+    <div>
+    <img className="rounded-full" src={`https://ui-avatars.com/api/?name=${props.nickname}&background=0D8ABC&color=fff`} alt="User Avatar" title="Avatar for Vanilla Leak User"/>
+    <button onClick={props.onClick}>Logout</button>
+    </div>
+  )
+}
+
+
