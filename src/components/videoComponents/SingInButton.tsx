92a64327ccaf68132b1dedd3b00276a3e0f1da2a
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-04-12 20:04:07 -0300

    change tag

--- src/components/videoComponents/SingInButton.tsx
+++ src/components/videoComponents/SingInButton.tsx
@@ -1,8 +1,8 @@
-
+import { Link } from "react-router-dom"
 export const SingInButton = () => {
   return (
     <div className="flex flex-col items-center">
-      <button className="border rounded-md bg-red-700 p-2 hover:bg-red-600">Sing In to comment</button>
+      <Link to="/auth/portal/signin" className="border rounded-md bg-red-700 p-2 hover:bg-red-600">Sing In to comment</Link>
     </div>
   )
 }
