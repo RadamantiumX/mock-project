0c489c2c13848699d8987361a4eb428d16d7e5d3
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-04-15 09:45:51 -0300

    setting local storage context

--- src/components/authComponents/SignInForm.tsx
+++ src/components/authComponents/SignInForm.tsx
@@ -6,10 +6,10 @@ import axiosClientAuth from "../../services/axios-client-auth"
 
 
 export const SignIn = () => {
-   const [email, setEmail] = useState("")
+   const [email, _setEmail] = useState("")
    const [password, setPassword] = useState("") 
 
-  const { setToken, setPath, setUser } = useStateContext()
+  const { setToken, setPath, setNickname, setEmail, setId } = useStateContext()
   
    const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
       e.preventDefault()
@@ -20,14 +20,13 @@ export const SignIn = () => {
 
         axiosClientAuth.post('/signin', payload)
           .then(({data}):any => {
+            // Set the local Storage
             setToken(data.response.token)
             setPath('home')
-            const user = {
-                id: data.response.id,
-                nickname: data.response.nickname,
-                email: data.response.email
-            }
-            setUser(user)
+            setNickname(data.response.nickname)
+            setEmail(data.response.email)
+            setId(data.response.id)
+            // Set the local Storage
           })
           .catch(err => {
             const response = err.response
@@ -42,7 +41,7 @@ export const SignIn = () => {
   return (
     <form className="flex flex-col gap-2 w-1/2" onSubmit={handleSubmit}>
       <h1 className="text-3xl">Login in to <span className="font-bold text-red-500">Vanilla Leak</span></h1>
-      <input type="email" name="email" placeholder="email" value={email} onChange={(e)=>setEmail(e.target.value)} required/>
+      <input type="email" name="email" placeholder="email" value={email} onChange={(e)=>_setEmail(e.target.value)} required/>
       <input type="password" name="password" placeholder="password" value={password} onChange={(e)=>setPassword(e.target.value)} required/>
       <Link to="/auth/portal/forgotten-password">¿Forgot your password?</Link>
       <button type="submit" className="border rounded-md">Sign In</button>
