6ddeb777e4cc5aadfe0d7bb327e635fa53487a71
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-04-15 10:12:35 -0300

    fix

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -1,15 +1,10 @@
 import { Outlet, Navigate } from "react-router-dom"
 import { useStateContext } from "../contexts/ContextProvider"
-// import { useEffect } from "react"
-// import axiosClientAuth from "../services/axios-client-auth"
 
 export default function AuthLayout() {
  const { token, age } = useStateContext()
 
-/*const payload = {
-  token: token
-}*/
-  
+
   if(token){
     return <Navigate to="/redirect"/>
   }
@@ -17,14 +12,6 @@ export default function AuthLayout() {
   if(!age){
     return <Navigate to="/rta"/>
   }
-
-/*useEffect(()=>{
-   axiosClientAuth.post('/token', payload)
-   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-   .then(({data}):any=>{
-     console.log(data)
-   })
-},[payload])*/
 
 
   return (
