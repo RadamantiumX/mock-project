adcbc8d120b3c3d73f7ecb4866b27e0ecafb1d9a
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-04-15 09:44:42 -0300

    adding more context states in local storage

--- src/contexts/ContextProvider.tsx
+++ src/contexts/ContextProvider.tsx
@@ -1,10 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { createContext, useContext, useState, PropsWithChildren, SetStateAction, Dispatch } from "react";
-import { User } from "../types/api";
+
 
 type ContextType = {
-    user: User | null,
-    setUser: Dispatch<SetStateAction<User | null>>, // Mandatory Types
+    nickname: string | null,
+    setNickname: Dispatch<SetStateAction<string | null>>, // Mandatory Types
+    email: string | null,
+    setEmail: Dispatch<SetStateAction<string | null>>, // Mandatory Types
+    id: number | null,
+    setId: Dispatch<SetStateAction<number | null>>, // Mandatory Types
     token: string | null,
     setToken: Dispatch<SetStateAction<string | null>>, // Mandatory Types
     path: string | null,
@@ -14,19 +18,25 @@ type ContextType = {
 }
 
 const StateContext = createContext<ContextType>({
-    user: null,
+    nickname: null,
+    email: null,
+    id: null,
     token: null,
     path: null,
     age: null,
 
-    setUser: () => {},
+    setNickname: () => {},
+    setEmail: () => {},
+    setId: () => {},
     setToken: () => {},
     setPath: () => {},
     setAge: () => {}
 })
 
 export const ContextProvider = ({ children }:PropsWithChildren) => {
-    const [user, _setUser] = useState<User>(localStorage.getItem('auth-user')) 
+    const [nickname, _setNickname] = useState(localStorage.getItem('auth-nickname')) 
+    const [email, _setEmail] = useState(localStorage.getItem('auth-email')) 
+    const [id, _setId] = useState<any>(localStorage.getItem('auth-id')) 
     const [token, _setToken] = useState(localStorage.getItem('auth-token'))
     const [path, setPath] = useState<any>("")
     const [age, _setAge] = useState(localStorage.getItem('rta'))
@@ -36,20 +46,37 @@ export const ContextProvider = ({ children }:PropsWithChildren) => {
         if(token){
             localStorage.setItem('auth-token', token)
         }else{
-            localStorage.removeItem('auth-token')
-            localStorage.removeItem('auth-user')
+            localStorage.clear() // Remove all local storage items
         }
     }
 
-    const setUser = (user:User) => {
-        _setUser(user)
-        if(user){
-            localStorage.setItem('auth-user', user)
+    const setNickname = (nickname:any) => {
+        _setNickname(nickname)
+        if(nickname){
+            localStorage.setItem('auth-nickname', nickname)
         }else{
-            localStorage.removeItem('auth-user')
+            localStorage.removeItem('auth-nickname')
+        }
+    }
+    
+
+    const setEmail = (email:any) => {
+        _setEmail(email)
+        if(email){
+            localStorage.setItem('auth-email', email)
+        }else{
+            localStorage.removeItem('auth-email')
         }
     }
 
+    const setId = (id:any) => {
+        _setId(id)
+        if(id){
+            localStorage.setItem('auth-id', id)
+        }else{
+            localStorage.removeItem('auth-id')
+        }
+    }
     const setAge = (age:any) => {
         _setAge(age)
         if(age){
@@ -60,7 +87,7 @@ export const ContextProvider = ({ children }:PropsWithChildren) => {
     }
 
     return (
-        <StateContext.Provider value={{ user, setUser, path, setPath, age, setAge, token, setToken }}>
+        <StateContext.Provider value={{ nickname, setNickname, path, setPath, age, setAge, token, setToken, email, setEmail, id, setId }}>
             {children}
         </StateContext.Provider>
     )
