a29c1cdfdf07c7bf552f9cf48d87ea0d2ab7cc98
Juan Arede <85207414+juanarede@users.noreply.github.com>
2024-04-15 10:14:19 -0300

    footer

--- src/components/commonComponents/LoadButton.tsx
+++ src/components/commonComponents/LoadButton.tsx
@@ -8,11 +8,22 @@
 
 export const LoadButton = (props: {onClick:()=>void, title: string}) => {
   return (
-    <div className="flex flex-col items-center mb-10">
-        <button className="border rounded-md bg-red-700 p-2" onClick={props.onClick}>
-            {props.title}
-        </button>
-    </div>
+
+    <div style={{marginBottom:"6rem"}} className="flex justify-center ">
+    <button 
+      type="button" 
+      className="ml-4 text-white bg-[#dd2471] hover:bg-[#dd2471]/80 focus:ring-4 focus:ring-[#FF9119]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#dd2471]/80 dark:focus:ring-[#dd2471]/40 mr-2 mb-2"
+      onClick={props.onClick}
+    >
+      {props.title}
+    </button>
+  </div>
+  
+    // <div style={{marginBottom:"9rem"}} className="flex flex-col items-center ">
+    //     <button className="border rounded-md bg-red-700 p-2" onClick={props.onClick}>
+    //         {props.title}
+    //     </button>
+    // </div>
   )
 }
 
