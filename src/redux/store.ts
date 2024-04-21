d74f744831366288e9bdd7ccec88ce00b20a9d4a
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-04-21 00:10:59 -0300

    delete unused reducer and middleware

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,22 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import sourceReducer from './epornerSources/sourceSlice'
-import authReducer from './authSources/authSlice'
 import favsReducer from './favSources/favsSlice'
-import { persistanceMiddleware } from './middleware'
+
 
 export const store = configureStore({
   reducer: {
     source: sourceReducer,
-    auth: authReducer,
     favs: favsReducer
   },
-  middleware: (getDefaultMiddleware) =>
-     getDefaultMiddleware({
-      thunk: {
-        extraArgument: persistanceMiddleware
-      },
-      serializableCheck: false
-     }) 
+ 
 })
 
 export type RootState = ReturnType<typeof store.getState>
