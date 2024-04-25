bd8ae707ec21ddaf9f5599dc4400aa0a98e9042a
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-04-25 11:48:39 -0300

    fetch post

--- src/components/videoComponents/PostCommentForm.tsx
+++ src/components/videoComponents/PostCommentForm.tsx
@@ -1,15 +1,43 @@
+import { MessageCircle } from "../icons/MessageCircle"
+import axiosClientAuth from "../../services/axios-client-auth"
+import { useState, useEffect } from "react"
+import { useStateContext } from "../../contexts/ContextProvider"
+
 export const PostCommentForm = () => {
+  const { token, videoId } = useStateContext()
+  const [content, setContent] = useState('')
+  const [response, setResponse] = useState<string| null>('')
+  const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const payload = {
+        token: token,
+        content: content,
+        videoId: videoId
+    }
+      await axiosClientAuth.post('/post/newpost',payload)
+       .then(({data})=>{
+         setResponse(data.response.message)
+       })
+       .catch( err => {
+         const res = err.response
+         console.log(res)
+       })
+  }
+
+  useEffect(()=>{
+    console.log(videoId)
+  },[])
   return (
     
-  <form>
+  <form onSubmit={handleSubmit}>
 <div id="commentsContainer" className="w-full md:w-1/2 p-2 pt-4 rounded shadow-lg mx-auto sm:ml-4">
   <div className="flex">
     <div>
-      <h2 className="font-semibold"><i className="fa-regular fa-comment mr-2 ml-3"></i> Comment <span className="font-bold">19</span> </h2>
+      <h2 className="font-semibold flex flex-row gap-1"><MessageCircle/> Comment </h2>
     </div>
   </div>
   <div className="mt-3 p-3">
-    <textarea id="comment" className="border p-2 rounded w-full" placeholder="Write your comment here..."></textarea>
+    <textarea id="content" value={content} onChange={(e)=>setContent(e.target.value)} className="border p-2 rounded w-full" placeholder="Write your comment here..."></textarea>
   </div>
   <div className="flex justify-between mx-3">
     <div>
