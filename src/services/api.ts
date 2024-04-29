c2c079cae73bcd70d9bd2dd7ec30e30086135a69
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-04-28 22:37:25 -0300

    change the method to GET

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,8 @@
 // import { FavsPayload } from "../types/api"
 
 // Recovery FAVS videos from current user
+
+import  { type APIVanillaLeakResponse } from "../types/api"
 export const isFav = async ( {payload}:any) => {
 
         const response = await fetch('http://localhost:4000/social/isfav',{
@@ -17,13 +19,9 @@ export const isFav = async ( {payload}:any) => {
 }
 
 // Recovery POST from current video
-export const latestPosts = async (payload:string | null) => {
-    const response = await fetch('http://localhost:4000/post/allpost',{
-            method: 'POST',
-            headers: { 'Content-Type' : 'application/json' },
-            body: JSON.stringify(payload)    
-        })
-
-     const data = await response.json()
+export const latestPosts = async (payload:string) => {
+    const response = await fetch(`http://localhost:4000/post/allpost/${payload}`)
+     const data = await response.json() as APIVanillaLeakResponse
+     // const {count:qty} = await response.json() as APIVanillaLeakResponse
      return data
 }
