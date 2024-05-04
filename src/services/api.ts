18258d2cbf1ee110c500040cea0ba057cbf15d65
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-05-03 23:57:13 -0300

    change on fetch

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,8 @@
 
 // Recovery FAVS videos from current user
 
-import  { type APIVanillaLeakResponse } from "../types/api"
+import  { type APIVanillaLeakPost } from "../types/api"
+import { type APIVanillaLeakReplys } from "../types/api"
 export const isFav = async ( {payload}:any) => {
 
         const response = await fetch('http://localhost:4000/social/isfav',{
@@ -20,8 +21,16 @@ export const isFav = async ( {payload}:any) => {
 
 // Recovery POST from current video
 export const latestPosts = async (payload:string) => {
-    const response = await fetch(`http://localhost:4000/post/allpost/${payload}`)
-     const data = await response.json() as APIVanillaLeakResponse
-     // const {count:qty} = await response.json() as APIVanillaLeakResponse
+    const response = await fetch(`http://localhost:4000/post/allpost/${payload}/post`)
+     const data = await response.json() as APIVanillaLeakPost
      return data
 }
+
+// Recovery Responses Posts from current video
+export const latestReplys = async () => {
+    const response = await fetch(`http://localhost:4000/post/allresponses`)
+     const {responses:data} = await response.json() as APIVanillaLeakReplys
+     return data
+}
+
+
