72e7d7f37806c9156f388197b5ed630b176ff482
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-05-03 23:59:10 -0300

    adding props

--- src/components/videoComponents/PostCommentForm.tsx
+++ src/components/videoComponents/PostCommentForm.tsx
@@ -5,10 +5,16 @@ import { useStateContext } from "../../contexts/ContextProvider"
 import { useAppDispatch } from "../../redux/hooks"
 import { getPostsSource } from "../../redux/postSources/postsSlice"
 
-export const PostCommentForm = () => {
+interface Props{
+   title: string
+}
+
+export const PostCommentForm:React.FC<Props> = ({title}) => {
   const { token, videoId, setNotification } = useStateContext()
   const [content, setContent] = useState('')
   const dispatch = useAppDispatch()
+   
+
   const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     const payload = {
@@ -31,7 +37,7 @@ export const PostCommentForm = () => {
 
   useEffect(()=>{
    // console.log(videoId)
-  },[videoId])
+  },[videoId, token])
   return (
     
   <form onSubmit={handleSubmit}>
@@ -39,19 +45,19 @@ export const PostCommentForm = () => {
 <div id="commentsContainer" className="w-full md:w-1/2 p-2 pt-4 rounded shadow-lg mx-auto sm:ml-4">
   <div className="flex flex-col">
     <div>
-      <h2 className="font-semibold flex flex-row gap-1"><MessageCircle/> Comment </h2>
+      <h2 className="font-semibold flex flex-row gap-1"><MessageCircle/> {title} </h2>
     </div>
     
   </div>
 
   <div className="mt-3 p-3">
-    <textarea id="content" value={content} onChange={(e)=>setContent(e.target.value)} className="border p-2 rounded w-full" placeholder="Write your comment here..."></textarea>
+    <textarea id="content" value={content} onChange={(e)=>setContent(e.target.value)} className="border p-2 rounded w-full resize-none" placeholder={`Write your ${title.toLowerCase()} here...`}></textarea>
   </div>
   <div className="flex justify-between mx-3">
     <div>
       <button type="submit" className="w-full md:w-auto px-4 py-1 bg-gray-800 text-white rounded font-light hover:bg-gray-700">Submit</button>
     </div>
-    <div></div>
+    
   </div>
 </div>
 </form>  
