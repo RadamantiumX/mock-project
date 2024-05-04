c2f30f9d98698689be5f357b73267cd456acbf8c
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-05-03 23:57:57 -0300

    adding slice on store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,13 +2,15 @@ import { configureStore } from '@reduxjs/toolkit'
 import sourceReducer from './epornerSources/sourceSlice'
 import favsReducer from './favSources/favsSlice'
 import postsReducer from './postSources/postsSlice'
+import replysReducer from './replySources/replysSlice'
 
 
 export const store = configureStore({
   reducer: {
     source: sourceReducer,
     favs: favsReducer,
-    posts: postsReducer
+    posts: postsReducer,
+    replys: replysReducer
   },
  
 })
