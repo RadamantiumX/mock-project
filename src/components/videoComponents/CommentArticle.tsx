38596d95c44c16eb64566dce0e6b442f56233eba
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-05-03 23:59:30 -0300

    some test

--- src/components/videoComponents/CommentArticle.tsx
+++ src/components/videoComponents/CommentArticle.tsx
@@ -1,28 +1,59 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Like } from "../icons/Like";
 import { Response } from "../icons/Response";
-import { useEffect } from "react";
+import { ResponsePost } from "../icons/ResponsePost";
+import { Trash } from "../icons/Trash";
+import { useEffect, useState } from "react";
 import moment from "moment";
+import { useStateContext } from "../../contexts/ContextProvider";
+import { useNavigate } from "react-router-dom";
+import { ReponsePostForm } from "./ReponsePostForm";
+import { ResponseArticle } from "./ResponseArticle";
+import {  useAppSelector } from "../../redux/hooks";
+
+
 interface Props {
    id: number;
    created: string;
-   nickname: string;
+   nick_name: string;
    content: string;   
 }
 
-export const CommentArticle: React.FC<Props>=  ({id, created, nickname, content}) => {
+export const CommentArticle: React.FC<Props>=  ({id, created, nick_name, content}) => {
+  const [showForm, setShowForm] = useState(false)
+  const [showArticle, setShowArticle] = useState(false)
+  const [currentUser, setCurrentUser] = useState(false)
+  const { setPostId, token, nickname } = useStateContext()
+  const navigate = useNavigate()
+  const data:any = useAppSelector( state => state.replys.data )
 
-  useEffect(()=>{
+ 
+ 
+  const handleFormResponse = () => {
+    if (token){
+       !showForm ? setShowForm(true) : setShowForm(false)
+    }else{
+      navigate('/auth/portal/signin')
+    }
+  }
 
-  },[id, nickname, content])
+
+
+  useEffect(() =>{
+   setPostId(id)
+   console.log(data)
+   if (nickname === nick_name)setCurrentUser(true)  
+   
+  },[id])
   return (
 <>
-<article className="relative flex antialiased" key={id}>
+<article className="relative flex antialiased" key={id} id="article">
   <div className="container px-0 mx-auto">
     <div className="flex-col w-full py-4 mx-auto sm:py-4 md:px-4 sm:rounded-lg sm:shadow-sm ">
       <div className="flex flex-row flex-wrap">
         <img className="object-cover w-12 h-12 border-2 border-gray-300 rounded-full ml-3" alt="Noob master's avatar" src="https://quarantine.doh.gov.ph/wp-content/uploads/2016/12/no-image-icon-md.png" />
         <div className="flex-col mt-1">
-          <div className="flex items-center flex-1 px-4 font-bold leading-tight text-white">{nickname}
+          <div className="flex items-center flex-1 px-4 font-bold leading-tight text-white">{nick_name}
             <span className="ml-2 text-xs font-normal text-gray-500">{moment(created, "YYYYMMDD hhmmss").fromNow()}</span>
           </div>
           <div className="flex">
@@ -32,16 +63,26 @@ export const CommentArticle: React.FC<Props>=  ({id, created, nickname, content}
               </div>
             </div>
           </div>
-          <button className="inline-flex items-center px-1 pt-2 ml-1 flex-column" aria-label="button response">
-           <Response/>
+          <button onClick={handleFormResponse} className="inline-flex items-center px-1 pt-2 ml-1 flex-column" aria-label="button response">
+           <Response />
           </button>
           <button className="inline-flex items-center px-1 -ml-1 flex-column" aria-label="button like">
             <Like/>
           </button>
+          {currentUser &&<button className="inline-flex items-center px-1 -ml-1 flex-column" aria-label="button trash">
+            <Trash/>
+          </button>}
+          <button onClick={()=>{ !showArticle ? setShowArticle(true) : setShowArticle(false) }} className="inline-flex items-center px-1 -ml-1 flex-column" aria-label="button like">
+            {/*data.count > 0 &&<div className="flex flex-row"><ResponsePost/> {data.count}</div>*/}
+          </button>
+          
         </div>
       </div>
     </div>
+    {showForm && <div className="ms-20"><ReponsePostForm id={id}/></div>}
+    {/*showArticle && data.reply?.length > 0 ? data.reply.map((item : { id: number; nickname: string; content: string; createdAt: string }) => (<div className="ms-20" key={item.id}><ResponseArticle user={currentUser} id={item.id} created={item.createdAt} _nickname={item.nickname} content={item.content}/></div>)) : <div></div> */}
   </div>
+  
 </article>
     </>
   );
--- src/components/videoComponents/Comments.tsx
+++ src/components/videoComponents/Comments.tsx
@@ -2,6 +2,7 @@
 import { CommentArticle } from "./CommentArticle"
 import { useAppDispatch, useAppSelector } from "../../redux/hooks"
 import { getPostsSource } from "../../redux/postSources/postsSlice"
+import { getReplysSource } from "../../redux/replySources/replysSlice"
 import { useEffect } from "react"
 import { useStateContext } from "../../contexts/ContextProvider"
 
@@ -13,14 +14,14 @@ export const Comments = () => {
 
 useEffect(()=>{ 
   dispatch(getPostsSource(videoId))
-  // console.log(posts)
+  dispatch(getReplysSource())
 },[videoId])
   return (
     <section className="flex flex-col  mt-5 mb-5">
         <div className="flex flex-col">
            {
              data.posts?.length > 0 ? data.posts.map((item: { id: number; nickname: string; content: string; createdAt: string })=> (
-              <div key={item.id}><CommentArticle  id={item.id} created={item.createdAt} nickname={item.nickname} content={item.content} /> </div>          
+              <div key={item.id}><CommentArticle  id={item.id} created={item.createdAt} nick_name={item.nickname} content={item.content} /> </div>          
              )): <h5 className="ml-5 text-2xl">No comments yet... Be the first 😎</h5>
            }
         </div>
