4f092deac448a0e841ea37337a57841e06c5e5ba
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-05-11 18:08:41 -0300

    videos fav listing

--- src/components/favVideosComponents/VideoFavCard.tsx
+++ src/components/favVideosComponents/VideoFavCard.tsx
@@ -8,9 +8,12 @@ interface Props {
 export const VideoFavCard:React.FC<Props> = ({id, default_thumb, title}) => {
   return (
     <article className="m-2">
-        <div className="flex flex-row gap-5 text-center" key={id}>
+        <div className="flex flex-row gap-5" key={id}>
             <img className="border rounded-md" src={default_thumb} alt={title} />
+            <div className="flex flex-col">
             <h5 className="text-justify">{title}</h5>
+            <p>1 month ago</p>
+            </div>
         </div>
     </article>
   )
--- src/components/favVideosComponents/VideosList.tsx
+++ src/components/favVideosComponents/VideosList.tsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { JSXElementConstructor, ReactElement, ReactNode, ReactPortal, useEffect, useState } from "react"
 import { VideoFavCard } from "./VideoFavCard"
 
 export const VideosList = () => {
-const [request, setRequest] = useState([])
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const [request, setRequest] = useState<any>([])
 
   const fetchTest = async () => {
      const req = await fetch('https://www.eporner.com/api/v2/video/search/?query=4k&per_page=12&page=2&thumbsize=small&gay=1&lq=1&format=json&order=latest')
@@ -11,13 +13,15 @@ const [request, setRequest] = useState([])
   }
   useEffect(()=>{
    fetchTest()
-   console.log(request)
+
   },[])  
   return (
     <section>
-         {request?.map((item, key) => (
-            <div className="flex flex-row gap-y-5 justify-center">{key}<VideoFavCard id={item.id} default_thumb={item.default_thumb.src} title={item.title}/></div>
+       <div className="flex flex-col justify-center">
+         {request?.map((item: { id: string; default_thumb: { src: string }; title: string }, key: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined) => (
+            <div className="flex flex-row gap-y-5 items-center justify-center">{key}<VideoFavCard id={item.id} default_thumb={item.default_thumb.src} title={item.title}/></div>
          ))}
+         </div>
     </section>
   )
 }
