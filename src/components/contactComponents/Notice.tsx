b50fea32696bbfd3bd992093896bd03ca81c1e1a
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-05-13 20:18:39 -0300

    adding link

--- src/components/contactComponents/Notice.tsx
+++ src/components/contactComponents/Notice.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 export const Notice = () => {
   return (
     <div>
-        <p>By submitting this, I confirm that I have read and understood the Privacy Policy.</p>
+        <p>By submitting this, I confirm that I have read and understood the <Link className="text-blue-700 hover:text-blue-800" to="/legal/privacy">Privacy Policy.</Link></p>
     </div>
   )
 }
