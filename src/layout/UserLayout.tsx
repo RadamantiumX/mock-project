2569eb83c49a6dbb6c88226de022d60f193c1926
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-05-13 20:18:19 -0300

    token verification in user panel

--- src/layout/UserLayout.tsx
+++ src/layout/UserLayout.tsx
@@ -1,8 +1,25 @@
 import NavBar from "../components/layoutComponents/NavBar"
 import Footer from "../components/layoutComponents/Footer"
-import { Outlet } from "react-router-dom"
+import { Outlet, Navigate } from "react-router-dom"
+import axiosClientAuth from "../services/axios-client-auth"
+import { useStateContext } from "../contexts/ContextProvider"
+
 export default function UserLayout() {
-  
+  const { token, setNotification } = useStateContext()
+
+  if(!token){
+    return <Navigate to="/redirect"/>
+  }else{
+    axiosClientAuth.post('/auth/token', {token: token})
+      .then(({data})=>{
+         setNotification(data.message)
+      })
+      .catch(err => {
+        const res = err.response
+        setNotification(res.message)
+      })
+  }
+
   return (
     <>
     <NavBar/>
