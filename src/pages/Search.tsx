c62f921799d385e95178e257a4c69f2a937a9b2a
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-06-04 11:36:30 -0300

    change params for querys

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,18 +1,18 @@
 import { VideosResults } from "../components/searchComponents/VideosResults"
-import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import { NotResults } from "../components/commonComponents/NotResults"
 import { useAppDispatch, useAppSelector } from "../redux/hooks"
 import { getEpornerSource } from "../redux/epornerSources/sourceSlice"
 import { LoadButton } from "../components/commonComponents/LoadButton"
+import { useQuery } from "../customsHooks/customsHooks"
 
-type Params = {
-  query: string
-}
+
 export default function Search() {
   const [counter, setCounter] = useState<number>(7)
-  const { query } = useParams<Params>()
-  const replaceSpace = query?.replace(/ /gi, "") // Replace white spaces to unify the string 
+  const  query = useQuery()
+  const search = query.get("query")
+  console.log(search)
+  const replaceSpace = search?.replace(/ /gi, "") // Replace white spaces to unify the string 
   
   const dispatch = useAppDispatch()
   const eporner = useAppSelector(state => state.source.data)
@@ -30,11 +30,11 @@ export default function Search() {
     
   }
 
-  },[query, counter]) // ✅Component data is refreshed when "query" is updated
+  },[search, counter]) // ✅Component data is refreshed when "query" is updated
 
   return (
     <main>
-     <h1 className="text-2xl sm:text-3xl md:text-2xl lg:text-3xl xl:text-3xl text-center sm:text-left ml-5 mt-9">Results of search for: <span className="font-bold text-pink-600">{query}</span></h1>
+     <h1 className="text-2xl sm:text-3xl md:text-2xl lg:text-3xl xl:text-3xl text-center sm:text-left ml-5 mt-9">Results of search for: <span className="font-bold text-pink-600">{search}</span></h1>
       {
         eporner?.length !== 0 ? 
         <div className="mt-8">
