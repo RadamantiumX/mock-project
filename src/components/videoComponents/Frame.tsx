29b080e4abdbce067e908d9ba0632895ef7b2c8e
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-06-06 11:36:07 -0300

    conditional for embed videos

--- src/components/videoComponents/Frame.tsx
+++ src/components/videoComponents/Frame.tsx
@@ -1,16 +1,26 @@
+import { useEffect, useState } from "react"
 interface Props {
     id?: string 
 }
 
 export const Frame:React.FC<Props> = ({ id }) => {
+  const [redtubeId, setRedtubeId] = useState("")
+
+  useEffect(()=>{
+    if(id?.includes("redtube")){
+        const arrayId = id.split("redtube")
+        setRedtubeId(arrayId[0])
+    }
+  },[redtubeId])
   return (
     <div className="relative overflow-hidden" style={{ maxWidth: "1150px", width: "100%" }}>
             <div style={{ paddingBottom: "54.25%", position: "relative" }}>
               <iframe
                 className="absolute top-0 left-0 w-full h-full desktop-height"
-                src={`https://eporner.com/embed/${id}`}
-                allow="accelerometer;autoplay;clipboard-write;encrypted-media;gyroscope;picture-in-picture"
+                src={!id?.includes("redtube")?`https://eporner.com/embed/${id}`: `https://embed.redtube.com/?id=${redtubeId}`}
+                allow="accelerometer;autoplay;clipboard-write;encrypted-media;gyroscope;picture-in-picture;gamepad;geolocation;layout-animations"
                 allowFullScreen
+                loading="lazy"
               ></iframe>
             </div>
           </div>
