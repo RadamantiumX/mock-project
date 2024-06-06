9ee677e55537136f1e1a9df43f98c81ea3111946
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-06-06 11:35:37 -0300

    adding model videos to test data

--- src/pages/ModelIndex.tsx
+++ src/pages/ModelIndex.tsx
@@ -1,29 +1,44 @@
-// import { getModelVideos } from "../services/resources"
+import { getModelVideos } from "../services/resources"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import axiosClientAuth from "../services/axios-client-auth"
 import { type ModelInfoDetail } from "../types/phubScrapingData"
 import { ModelCardIndex } from "../components/modelIndexComponents/ModelCardIndex"
+import { ModelVideosRelated } from "../components/modelIndexComponents/ModelVideosRelated"
+
+
 
 export default function ModelIndex() {
  const { name } = useParams<string>() 
   const [model, setModel] = useState<ModelInfoDetail>()
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [modelData, setModelData] = useState<any>([])
 
 
 useEffect(()=>{
   axiosClientAuth(`/phub/model-info/${name}`)
    .then((data)=>{
-     console.log(data.data)
+
      setModel(data.data)
    })
    .catch(err=>{
     console.log(err)
    })
+
+   getModelVideos(name)
+     .then((data)=>{
+        console.log(data)
+        setModelData(data)
+     })
+     .catch(err=>{
+      console.log(err)
+     })
 },[name])
   return (
     <main>
       <section>
         <ModelCardIndex name={model?.name} cover={model?.cover} about={model?.about} avatar={model?.avatar} height={model?.height} weight={model?.weight} gender={model?.gender}/> 
+        <ModelVideosRelated source={modelData}/>
       </section>
        
     </main>
