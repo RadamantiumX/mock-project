1dea2b57b867b847ff9be5605c807e46d0e3b696
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-06-06 11:35:04 -0300

    some fix into 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -7,8 +7,8 @@ export default function NotFound() {
         style={{ backgroundImage: "url('https://external-preview.redd.it/4MddL-315mp40uH18BgGL2-5b6NIPHcDMBSWuN11ynM.jpg?width=960&crop=smart&auto=webp&s=b98d54a43b3dac555df398588a2c791e0f3076d9'" }}>
         <div className="max-w-md mx-auto text-center bg-white bg-opacity-90 p-8 rounded-lg shadow-lg">
           <img src={Logo} alt="logo" />
-          <div className="text-9xl font-bold text-pink-600 mb-4">404</div>
-          <h1 className="text-4xl font-bold text-gray-800 mb-6">Oops! Page Not Found</h1>
+          <div className="text-9xl font-bold text-pink-600 mb-4">Oops!</div>
+          <h1 className="text-4xl font-bold text-gray-800 mb-6">404 Page Not Found</h1>
           <p className="text-lg text-gray-600 mb-8">The page you're looking for seems to have gone on a little adventure. Don't
             worry, we'll help you find your way back home.</p>
           <Link to="/"
