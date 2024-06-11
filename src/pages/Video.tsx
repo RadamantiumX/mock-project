d99c9be69af9c9aaba592b47237802fef2df12c0
Juan Arede <85207414+juanarede@users.noreply.github.com>
2024-06-11 18:20:08 -0300

    Skeleton agregado

--- src/pages/Video.tsx
+++ src/pages/Video.tsx
@@ -2,11 +2,10 @@
 import { VideoSelected } from "../components/videoComponents/VideoSelected"
 import { useParams } from "react-router-dom"
 import { SideBarVideos } from "../components/videoComponents/SideBarVideos"
-import { PostCommentForm } from "../components/videoComponents/PostCommentForm"
+// import { PostCommentForm } from "../components/videoComponents/PostCommentForm"
 import { Comments } from "../components/videoComponents/Comments"
-import { SingInButton } from "../components/videoComponents/SingInButton"
-import { useStateContext } from "../contexts/ContextProvider"
-import { useEffect } from "react"
+// import { useStateContext } from "../contexts/ContextProvider"
+// import { useEffect } from "react"
 
 type Params = {
    id: string;
@@ -17,26 +16,19 @@ type Params = {
 export default function Video() {
   const { id, title, keywords } = useParams<Params>();
   const { views } = useParams() as any; /** Temporal FIX 😎 */
-  const { token, setVideoId } = useStateContext()
+  // const { token, setVideoId } = useStateContext()
 
-  useEffect(()=>{
-    setVideoId(id)
-  },[setVideoId])
+  // useEffect(()=>{
+  //   setVideoId(id)
+  // },[setVideoId])
   return (
     <main>
       <div className="gap-20">
         <VideoSelected id={id} title={title} views={views} />
         <SideBarVideos keywords={keywords} />
-        {token ? <PostCommentForm title={'Comment'}/>: <SingInButton/>}
+        {/* {token ? <PostCommentForm title={'Comment'}/> : null} */}
         <Comments />
       </div>
     </main>
   );
 }
-
-
-/**
- * ❗ Realizar una QUERY donde se obtengan resultados a partir de una query 
- * ❗ Utilizamos los "Keywords" de esa query para el fetch en related VIDEOS.
- * 
- */
