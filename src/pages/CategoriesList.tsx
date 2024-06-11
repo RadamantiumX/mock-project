b87ed680f61c96ac00c1f2ceb2a1335c4a644654
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-06-11 17:50:19 -0300

    adding components into cat list

--- src/pages/CategoriesList.tsx
+++ src/pages/CategoriesList.tsx
@@ -1,8 +1,11 @@
+import { Header } from "../components/categoriesListComponents/Header"
+import { AllCategories } from "../components/categoriesListComponents/AllCategories"
 
 export default function CategoriesList() {
   return (
     <main>
-      
+      <Header/>
+      <AllCategories/>
     </main>
   )
 }
