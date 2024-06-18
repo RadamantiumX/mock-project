fee519f5fb396facc86201eb24b400da60541421
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-06-17 23:12:39 -0300

    adding pagination comp

--- src/components/photosComponents/PicPages.tsx
+++ src/components/photosComponents/PicPages.tsx
@@ -1,30 +1,42 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axiosClientAuth from "../../services/axios-client-auth"
 import { useEffect, useState } from "react"
 import { type Datum } from "../../types/phubPicsData"
 import { AlbumCard } from "./AlbumCard"
+import { Pagination } from "../commonComponents/Pagination"
+import { useRange } from "../../customsHooks/customsHooks"
 
 interface Props{
   tag: string | null
+  page: string | any
 }
 
-export const PicPages:React.FC<Props> = ({tag}) => {
+export const PicPages:React.FC<Props> = ({tag, page}) => {
   const [pics, setPics] = useState<Datum[]>([])
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [pages, setPages] = useState<any>()
+  
+  const { rangePages } = useRange(1, pages)
+
   useEffect(()=>{
-    axiosClientAuth(`/phub/pics/1/${tag}`)
+    console.log(rangePages)
+    axiosClientAuth(`/phub/pics/${page}/${tag}`)
       .then((data)=>{
         console.log(data.data.pages)
+        setPages(data.data.pages)
         setPics(data.data.data)
       })
       .catch(err => {
         console.log(err)
       })
-  },[tag])
+  },[tag, pages])
   return (
     <>
     <section className="mt-20">
      <div className="grid grid-flow-row grid-cols-4">
         {pics.map((item)=>(<AlbumCard  title={item.title} preview={item.preview}/>))}
      </div>
+     <Pagination itemsPage={rangePages} currentPage={parseInt(page)}/>
     </section>
     </>
   )
