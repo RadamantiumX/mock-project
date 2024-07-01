be96a2e9962ba38db2df5cfeb12f9b2f920d47e2
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-06-30 23:03:02 -0300

    removing the infinite scroll for pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,21 @@
 import { Videos } from "../components/displayComponents/Videos";
-import { LoadButton } from "../components/commonComponents/LoadButton";
+// import { LoadButton } from "../components/commonComponents/LoadButton";
 import SkeletonLoader from "../components/commonComponents/SkeletonLoader"; 
 import { useHomeVideos } from "../customsHooks/customsHooks";
+import { useRange, useQuery } from "../customsHooks/customsHooks";
+import { Pagination } from "../components/commonComponents/Pagination";
 
 export default function Home() {
-
-  const {isLoading, eporner, handleResults} = useHomeVideos()
- 
+  const query = useQuery()
+  const currentPage = query.get('page')
+  const {isLoading, eporner } = useHomeVideos(currentPage === null ? 1 : parseInt(currentPage))
+  const { rangePages } = useRange(1, Math.round(eporner.total_pages))
   return (
     <main>
-      <Videos source={eporner} />
+      <Videos source={eporner?.videos} />
       {isLoading ? <SkeletonLoader /> : null} 
-      <LoadButton onClick={handleResults} title={"Load more Videos.."} />
+      {/*<LoadButton onClick={handleResults} title={"Load more Videos.."} />*/}
+      <Pagination itemsPage={rangePages} currentPage={currentPage === null ? 1: parseInt(currentPage)} optParam={null}/>
     </main>
   );
 }
