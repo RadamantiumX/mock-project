a3fdc6239ca8282b710f6a4fd5c88e8592f1b203
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-07-02 18:59:36 -0300

    fixing and cleaning code

--- src/services/resources.ts
+++ src/services/resources.ts
@@ -7,36 +7,29 @@ import {  type APIEpornerResponse } from "../types/eporner"
 // Eporner
 
 export const getLatestContent = async ( payload: number) => {
-    // Traer un numero de los parametros, si es menor o igual a 1, aumentamos "binary", sino, aumentamos "qty"
-    /*let qty = 12
-    let binary = 0
-    if (payload <= 1){
-        binary = payload
-    }else{
-        qty = qty + payload
-    }*/
-    const res = await fetch(`${import.meta.env.VITE_EPORNER_API_URL}api/v2/video/search/?page=${payload}&order=latest`)
+    const res = await fetch(`${import.meta.env.VITE_EPORNER_API_URL}api/v2/video/search/?page=${payload}&order=latest&gay=0`)
     const data = await res.json() as APIEpornerResponse
-   
-    return data
-   // Deleted: per_page=${qty.toString()}
+    return data  
 }
 
 // This function needs separate two values to modify the API response
 export const getRelatedVideos = async ( payload:string) => {
-    let qty = 7 // Default value to set quantity of results by request
-    const paramsArray = payload.split(' ') // Splits params in 2 elements
-    qty = qty + parseInt(paramsArray[1]) // Seting new value
-    const res = await fetch(`${import.meta.env.VITE_EPORNER_API_URL}api/v2/video/search/?query=${paramsArray[0]}&per_page=${qty.toString()}&page=2&thumbsize=big&order=latest&gay=1&lq=1&format=json`)
-    const {videos: data} = await res.json() as APIEpornerResponse
+    const res = await fetch(`${import.meta.env.VITE_EPORNER_API_URL}api/v2/video/search/?query=${payload}&per_page=10&order=latest`)
+    const data = await res.json() as APIEpornerResponse
     return data
   
 }
 
-export const getOrderVideos = async ({ payload }:any) => {
+export const getSearchContent = async ( query:string | null, page:number | null) => {
+    const res = await fetch(`${import.meta.env.VITE_EPORNER_API_URL}api/v2/video/search/?query=${query}&page=${page === null ? 1 : page}&order=latest&gay=0`)
+    const data = await res.json() as APIEpornerResponse
+    return data  
+}
+
+export const getOrderVideos = async (page:number | null, order:string | undefined) => {
     
-    const res = await fetch(`${import.meta.env.VITE_EPORNER_API_URL}api/v2/video/search/?query=4k&per_page=${payload.qty}&page=2&thumbsize=big&gay=1&lq=1&format=json&order=${payload.order}&gay=1&lq=1&format=json`)
-    const {videos: data} = await res.json() as APIEpornerResponse
+    const res = await fetch(`${import.meta.env.VITE_EPORNER_API_URL}api/v2/video/search/?page=${page}&order=${order}`)
+    const data = await res.json() as APIEpornerResponse
     return data
 }
 
