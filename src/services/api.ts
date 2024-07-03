72b555217877a1fc9dedd2f5af9911dba2528e3e
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-07-03 20:02:04 -0300

    fixing endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,7 +8,7 @@ import { type APIVanillaLeakReplys } from "../types/api"
 // import { type Like } from "../types/api"
 export const isFav = async ( {payload}:any) => {
 
-        const response = await fetch('http://localhost:4000/social/isfav',{
+        const response = await fetch('http://localhost:3000/social/isfav',{
             method: 'POST',
             headers: { 'Content-Type' : 'application/json' },
             body: JSON.stringify(payload)    
@@ -20,20 +20,20 @@ export const isFav = async ( {payload}:any) => {
 
 // Recovery POST from current video
 export const latestPosts = async (payload:string) => {
-    const response = await fetch(`http://localhost:4000/post/allpost/${payload}/post`)
+    const response = await fetch(`http://localhost:3000/post/allpost/${payload}/post`)
      const data = await response.json() as APIVanillaLeakPost
      return data
 }
 
 // Recovery Responses Posts from current video
 export const latestReplys = async () => {
-    const response = await fetch(`http://localhost:4000/post/allresponse`)
+    const response = await fetch(`http://localhost:3000/post/allresponse`)
      const {responses:data} = await response.json() as APIVanillaLeakReplys
      return data
 }
 
 export const isLike = async ({payload}:any) =>{
-    const response = await fetch('http://localhost:4000/like/current',{
+    const response = await fetch('http://localhost:3000/like/current',{
             method: 'POST',
             headers: { 'Content-Type' : 'application/json' },
             body: JSON.stringify(payload)    
