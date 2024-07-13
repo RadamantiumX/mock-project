8cdde3f5747f42ca031fc59d55a9f18b6b1e24da
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-07-13 00:03:38 -0300

    change the response

--- src/customsHooks/favVideosHooks.tsx
+++ src/customsHooks/favVideosHooks.tsx
@@ -2,26 +2,26 @@ import { useState, useEffect } from "react"
 import { useStateContext } from "../contexts/ContextProvider"
 import { type FavVideosProfile } from "../types/eporner"
 import axiosClientAuth from "../services/axios-client-auth"
-import { fetchTest } from "../services/resources"
+import { getFavVideos } from "../services/resources"
+import { useNavigate } from "react-router-dom"
 
 
 export const useFavVideos = () => {
-    const [request] = useState<FavVideosProfile[]>([])
+    const [request, setRequest] = useState<FavVideosProfile[]>([])
     const { token } = useStateContext()
 
     useEffect(()=>{
         axiosClientAuth.post('/social/fav-videos', {token})
      .then(({data})=>{
   
-      data.results.map((e:string)=>{
-        fetchTest(e)
-          .then((res)=>{
-              request.push(res)
-          })
-          .catch(error=>{
-            console.error(error)
-          })
-      })
+      getFavVideos([data.results])
+       .then((data)=>{
+         setRequest(data)
+       })
+       .catch(error=>{
+        console.error('Something went wrong!')
+        console.error(error.message)
+       })
           
      })
      .catch(error=>{
@@ -35,12 +35,16 @@ export const useFavVideos = () => {
 
 export const useDelFav = (videoId:string) => {
     const { setNotification, token } = useStateContext()
+    const navigate = useNavigate()
 
     const handleDeleteFav = () => {
         axiosClientAuth.post(`/social/delfav`, {videoId, token})
          .then(({data})=>{
             console.log(data)
             setNotification(data.message)
+            setTimeout(()=>{
+                navigate(0)
+            },2000)
          })
          .catch(error=>{
             console.error(error.message)
