47d68611b146223c32c60e6d81d622640d934dda
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-07-13 23:56:28 -0300

    customs hooks for profile files

--- src/customsHooks/profileHooks.tsx
+++ src/customsHooks/profileHooks.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 import { useStateContext } from "../contexts/ContextProvider"
 import axiosClientAuth from "../services/axios-client-auth"
+import { useNavigate } from "react-router-dom"
 
 type User = {
     nickname: string
@@ -20,4 +21,50 @@ export const useUserInfo=()=>{
     },[])
 
     return {user}
+}
+
+export const useShowFields = () => {
+  const [ showFirst, setShowFirst ] = useState(false)
+  const [ showSecond, setShowSecond ] = useState(false)
+
+  return { showFirst, setShowFirst, showSecond, setShowSecond }
+}
+
+export const useUpdateUserInfo = () => {
+  const [field, setField] = useState('')
+   const [payload, setPayload] = useState('')
+   const {token, setNotification, setToken, setNickname, setPath} = useStateContext()
+   const navigate = useNavigate()
+
+   const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault()  
+      axiosClientAuth.post('/user/user-update',{payload, field, token})
+       .then(({data})=>{
+        
+        setPayload('')
+        if(field === 'email'){
+          setNotification(data.message)
+          setTimeout(()=>{
+            localStorage.clear() // Clear full storage
+        setToken(null) // Token too
+        setNickname(null)
+        setPath('home')
+        navigate('/redirect')
+          },1000)
+        }else{
+          setNotification(data.message)
+          setTimeout(()=>{
+            
+            setNickname(payload)
+            navigate(0)
+          },2000)
+          
+        }
+       })
+       .catch(error=>{
+        console.error(error)
+       })
+   }
+
+   return { field, setField, payload, setPayload, handleSubmit }
 }
