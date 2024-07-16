3dd7925d771c23630fed61b8ed61db4cc4054a25
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-07-16 15:12:31 -0300

    lang const

---
+++ src/components/const/lang.ts
@@ -0,0 +1,16 @@
+export const LANG = {
+    en: {
+        "nav.home": "Home",
+        "nav.categories": "Categories",
+        "nav.models": "Models",
+        "nav.photos": "Photos",
+        "nav.videos": "Videos"
+    },
+    es:{
+        "nav.home": "Home",
+        "nav.categories": "Categorias",
+        "nav.models": "Modelos",
+        "nav.photos": "Fotos",
+        "nav.videos": "Videos"
+    }
+}
