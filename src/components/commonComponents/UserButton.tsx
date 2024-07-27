9916cf2e6585d95fc901ff4f264efa5ba3603be0
Juan Arede <85207414+juanarede@users.noreply.github.com>
2024-07-27 00:22:24 -0300

    mejoras de avatar y seleccion de lenguaje

--- src/components/commonComponents/UserButton.tsx
+++ src/components/commonComponents/UserButton.tsx
@@ -6,31 +6,81 @@ import { Logout } from "../icons/Logout";
 import { Settings } from "../icons/Settings";
 import { Hearth } from "../icons/Hearth";
 
-
-export const UserButton = (props: {onClick:()=>void, nickname: string | null}) => {
-  const { isOpen, toggleDropdown, handleLogout } = useToogleButton(props.onClick)
+export const UserButton = (props: { onClick: () => void, nickname: string | null, email: string | null }) => {
+  const { isOpen, toggleDropdown, handleLogout } = useToogleButton(props.onClick);
 
   return (
-<div className="relative z-10">
-  <button className="flex items-center space-x-2" onClick={toggleDropdown}>
-    <img className="rounded-full h-8 w-8" src={`https://ui-avatars.com/api/?name=${props.nickname}&background=0D8ABC&color=fff`} alt="User Avatar" title="Avatar for Vanilla Leak User"/>
-    <svg className="h-4 w-4 fill-current text-gray-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-      <path fillRule="evenodd" d="M10 12a1 1 0 01-.707-.293l-4-4a1 1 0 111.414-1.414L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-.707.293z" clipRule="evenodd"/>
-    </svg>
-  </button>
-  {isOpen && (
-    <div className="absolute right-0 mt-2 w-48 bg-gray-800 rounded-md shadow-lg">
-      <div className="py-1">
-        <button className="block px-2 py-1 text-white hover:bg-gray-800 w-full text-center" onClick={handleLogout}><div className='flex flex-row gap-1'><FormattedMessage id='common.userbtn.btn#1' defaultMessage="Logout"/><Logout/></div></button>
-        <Link className="block px-2 py-1 text-white hover:bg-gray-800 w-full text-center" reloadDocument to="/user/profile"><div className='flex flex-row gap-1'><FormattedMessage id='common.userbtn.btn#2' defaultMessage="Account Settings"/><Settings/></div></Link>
-        <Link className="block px-2 py-1 text-white hover:bg-gray-800 w-full text-center" reloadDocument to="/user/fav"><div className='flex flex-row gap-1'><FormattedMessage id='common.userbtn.btn#3' defaultMessage="Favorites"/><Hearth/></div></Link>
+    <div className="relative z-10">
+      <button
+        id="dropdownUserAvatarButton"
+        className="flex text-sm transition-transform duration-150 ease-in-out transform bg-gray-900 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600 active:scale-95"
+        type="button"
+        onClick={toggleDropdown}
+      >
+        <span className="sr-only">Open user menu</span>
+        <img
+          className="w-8 h-8 rounded-full"
+          src={`https://ui-avatars.com/api/?name=${props.nickname}&background=0D8ABC&color=fff`}
+          alt="User Avatar"
+        />
+      </button>
+
+      <div
+        id="dropdownAvatar"
+        className={`absolute right-0 z-10 w-48 mt-2 bg-white border divide-y divide-gray-100 rounded-lg shadow-lg dark:bg-black dark:divide-gray-600 transition transform origin-top-right ${isOpen ? 'scale-100 opacity-100' : 'scale-95 opacity-0 pointer-events-none'}`}
+      >
+        <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
+          <div>{props.nickname}</div>
+          <div className="font-medium truncate">{props.email}</div>
+        </div>
+        <ul
+          className="py-2 text-sm text-gray-700 dark:text-gray-200"
+          aria-labelledby="dropdownUserAvatarButton"
+        >
+          <li>
+            <Link
+              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              to="/user/profile"
+            >
+              <div className="flex flex-row gap-1">
+                <Settings />
+                <FormattedMessage
+                  id="common.userbtn.btn#2"
+                  defaultMessage="Account Settings"
+                />
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              to="/user/fav"
+            >
+              <div className="flex flex-row gap-1">
+                <Hearth />
+                <FormattedMessage
+                  id="common.userbtn.btn#3"
+                  defaultMessage="Favorites"
+                />
+              </div>
+            </Link>
+          </li>
+        </ul>
+        <div className="py-2">
+          <button
+            className="block w-full px-4 py-2 text-sm text-left text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+            onClick={handleLogout}
+          >
+            <div className="flex flex-row gap-1">
+              <Logout />
+              <FormattedMessage
+                id="common.userbtn.btn#1"
+                defaultMessage="Logout"
+              />
+            </div>
+          </button>
+        </div>
       </div>
     </div>
-  )}
-</div>
-
   );
 };
-
-
-
--- src/components/layoutComponents/Selection.tsx
+++ src/components/layoutComponents/Selection.tsx
@@ -1,17 +1,35 @@
 import { useLang } from "../../customsHooks/customsHooks";
-import { FormattedMessage } from "react-intl";
+import { IconWorld } from "../icons/IconWorld";
+import { useEffect } from "react";
 
 export const Selection = () => {
-  const {lang, handleSelect} = useLang()
-  
+  const { lang, handleSelect } = useLang();
+
+  useEffect(() => {
+    const options = document.querySelectorAll("#language option");
+    options.forEach(option => {
+      option.classList.add("bg-black", "text-white", "hover:border-gray-800");
+    });
+  }, []);
+
   return (
-    <div className="relative flex items-center flex-col">
-      <h5><FormattedMessage  id="lang" defaultMessage="Language"/></h5>
-      <select className="w-20" name="" id=""  onChange={handleSelect} defaultValue={lang}>
-        <option value="en">EN</option>
-        <option value="es">ES </option>
-      </select>
-      
+    <div className="relative flex flex-col justify-center h-full">
+    
+      <div className="relative w-20">
+        <select
+          className="block w-full pr-8 leading-tight bg-transparent shadow appearance-none hover:border-gray-500 focus:outline-none focus:shadow-outline"
+          name="language"
+          id="language"
+          onChange={handleSelect}
+          defaultValue={lang}
+        >
+          <option value="en">EN</option>
+          <option value="es">ES</option>
+        </select>
+        <div className="absolute inset-y-0 right-0 flex items-center pointer-events-none px-7">
+          <IconWorld />
+        </div>
+      </div>
     </div>
   );
 };
